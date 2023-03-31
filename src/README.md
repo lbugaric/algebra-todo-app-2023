d7c788e7e130f4fa0a347a96e6110426258081d7
lbugaric <125271754+lbugaric@users.noreply.github.com>
2023-03-31 18:47:11 +0200

    item list add p2

---
+++ src/README.md
@@ -0,0 +1,15 @@
+# Todo App
+
+## Getting started
+
+Install node 14+.
+
+1. npm install
+2. npm run dev
+
+## Features
+
+- [ ] Add a todo item
+- [x] List todo items
+- [ ] Remove todo item
+- [x] Mark todo item as done
---
+++ src/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const defaultItems = [
+ {
+   id: 1,
+   text: "Kupi mlijeko",
+   done: false,
+ },
+ {
+  id: 2,
+  text: "Kupi brašno",
+  done: true,
+  },
+];
+
+
+function App() {
+  const [items, setItems] = useState(defaultItems);
+
+  console.log(items);
+
+  const itemComponents = items.map(item => {
+    const handleChange = () => {
+      console.log('handle change for item', item);
+      setItems(items.map(newItem => {
+        if (newItem.id === item.id) {
+          return { ...newItem, done: !item.done };
+        }
+        return newItem;
+      }));
+    };
+
+    return(
+      <div key={item.id}>
+        <input type="checkbox" checked={item.done} onChange={handleChange} />
+        {item.text}
+      </div>
+    );
+  })
+
+  return (
+    <div>
+      <h1>To-do app</h1>
+      {itemComponents}
+    </div>
+  )
+}
+
+export default App
