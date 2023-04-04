682b4be3c102a45298667d23b14b664579ce9457
lbugaric <125271754+lbugaric@users.noreply.github.com>
2023-04-04 19:38:41 +0200

    Sort by date p2

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
 import { useState } from 'react'
-
-
+import { TodoItem } from './components/TodoItem';
+import { TodoItemForm } from './components/TodoItemForm';
 
 
 function App() {
   const [items, setItems] = useState([]);
-  const [formState, setFormState] = useState({
-    text: "",
-  });
   const [sort, setSort] = useState("createdAtDesc");
 
   const handleSortChange = (event) => {
     setSort(event.target.value);
   }
 
-  const handleChange = (event) => {
-    setFormState({
-      ...formState,
-      [event.target.name]: event.target.value,
-    });
+  const handleCreateItem = (item) => {
+    setItems([ ...items, item ]);
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setItems([
-      ...items,
-      {
-        id: Date.now(),
-        text: formState.text,
-        done:false,
-        createdAt: Date.now(),
-      },
-    ]);
-    setFormState({ ...formState, text: '' });
+  const handleClearItems = () => {
+    setItems([]);
   }
+
+  const handleMarkItemAsDone = (id, done) => {
+    setItems(items.map(newItem => {
+      if (newItem.id === id) {
+        return { ...newItem, done: !done };
+      }
+      return newItem;
+    }));
+  };
+
+  const handleDeleteItem = (id) => {
+    setItems(items.filter(newItem => {
+      return newItem.id !== id;
+    }));
+  };
 
   const itemComponents = items
   .sort((a, b) => {
@@ -44,61 +43,21 @@ function App() {
     return b.createdAt - a.createdAt;
   })
   .map(item => {
-    const handleChange = () => {
-      console.log('handle change for item', item);
-      setItems(items.map(newItem => {
-        if (newItem.id === item.id) {
-          return { ...newItem, done: !item.done };
-        }
-        return newItem;
-      }));
-    };
-
-    const handleClick = () => {
-      setItems(items.filter(newItem => {
-        return newItem.id !== item.id;
-      }));
-    };
-
-    return(
-      <div key={item.id}>
-        <input type="checkbox" checked={item.done} onChange={handleChange} />
-        {item.text} ({new Date(item.createdAt).toUTCString()})
-        <button onClick={handleClick}>X</button>
-      </div>
-    );
+    return <TodoItem key={item.id} id={item.id} done={item.done} text={item.text} createdAt={item.createdAt} onDeleteItem={handleDeleteItem} onMarkItemAsDone={handleMarkItemAsDone} />
   });
 
   return (
     <div>
       <h1>To-do app</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="text" onChange={handleChange} value={formState.text} />
-        <button type="submit">Add item</button>
-      </form>
+      <TodoItemForm onCreateItem={handleCreateItem} />
       <select onChange={handleSortChange} defaultValue={sort}>
         <option value="createdAtAsc">Created at (Ascending)</option>
         <option value="createdAtDesc">Created at (Descending)</option>
       </select>
+      <button onClick={handleClearItems}>Clear items</button>
       {itemComponents}
     </div>
   )
 }
 
 export default App
-
-
-/*
-const defaultItems = [
- {
-   id: 1,
-   text: "Kupi mlijeko",
-   done: false,
- },
- {
-  id: 2,
-  text: "Kupi brašno",
-  done: true,
-  },
-];
-*/
